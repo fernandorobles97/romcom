e412aae991dfd8ed9e9fdfbef5c859a163dc1a31
“Saki <sakisandra.c@gmail.com>
2023-04-02 13:40:26 -0600

    Remove stock cover from saved covers array

--- src/main.js
+++ src/main.js
@@ -18,9 +18,7 @@ var descriptor1Input = document.querySelector('#descriptor1');
 var descriptor2Input = document.querySelector('#descriptor2');
 var form = document.querySelector('form');
 var currentCover;
-var savedCovers = [
-  createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
+var savedCovers = [];
 
 // Add your event listeners here 
 window.addEventListener('load', randomCover);
